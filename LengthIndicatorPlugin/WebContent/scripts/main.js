a65ab516878294b9aa156b1ec9d24eca2406ceb4
Mike <mikenitchie@gmail.com>
2014-09-04 22:27:28 -0400

    Added examples and some basic styling.

--- LengthIndicatorPlugin/WebContent/scripts/main.js
+++ LengthIndicatorPlugin/WebContent/scripts/main.js
@@ -1,22 +1,4 @@
 'use strict'
 $(document).ready(function() {
-    $('input').lengthIndicator();
-
-//    $(document).on('keydown', function(event) {
-//        console.log('keydown ' + event.which);
-//        console.log('keydown ' + event.keyCode);
-//        console.log(event);
-//    });
-//    
-//    $(document).on('keypress', function(event) {
-//        console.log('keypress ' + event.which);
-//        console.log('keypress ' + event.keyCode);
-//        console.log(event);
-//    });
-//
-//    $(document).on('keyup', function(event) {
-//        console.log('keyup ' + event.which);
-//        console.log('keyup ' + event.keyCode);
-//        console.log(event);
-//    });
+    $('#demo').lengthIndicator();
 });
